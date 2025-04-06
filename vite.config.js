8278256710b687580c70e62b3a514c0d4f9516cb
Uyen (Chloe) Nguyen <thaouyen.nttu@gmail.com>
2025-04-06 00:59:07 -0400

    Change repo name

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/port-web/', // repo name
+  base: '/portfolio/', // repo name
   plugins: [react()],
 });
